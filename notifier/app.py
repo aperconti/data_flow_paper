c6418927ecd4460bfc1b965bd0a377eafa706f13
Autum Perconti <autumlucille@gmail.com>
2022-01-30 18:44:58 -0800

    adding try/catch + logging

--- notifier/app.py
+++ notifier/app.py
@@ -11,14 +11,19 @@ app = Chalice(app_name='notifier')
 def index(event):
     app.log.error("Received message with subject: %s, message: %s",
                   event.subject, event.message)
-    sg = sendgrid.SendGridAPIClient(api_key=os.environ["SENDGRID_API_KEY"])
-    from_email = Email("autumlucille@gmail.com")
-    to_email = To("autumlucille@gmail.com")
-    subject = "Sending with SendGrid is Fun"
-    content = Content(
-        "text/plain", "and easy to do anywhere, even with Python")
-    mail = Mail(from_email, to_email, subject, content)
-    response = sg.client.mail.send.post(request_body=mail.get())
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
+
+    try:
+        student_email = event.message.get('student_email')
+        sg = sendgrid.SendGridAPIClient(api_key=os.environ["SENDGRID_API_KEY"])
+        from_email = Email("autumlucille@gmail.com")
+        to_email = To(student_email)
+        subject = "We've received your paper!"
+        content = Content(
+            "text/plain", "Congrats on submitting your paper! We wanted to know that we've alerted all tutors and should have you matched with one shortly!")
+        mail = Mail(from_email, to_email, subject, content)
+        response = sg.client.mail.send.post(request_body=mail.get())
+        app.log.debug("Error sending message to sendgrid: %s, message: %s response: %s",
+                      event.subject, event.message, response)
+    except:
+        app.log.error("Error sending message to sendgrid: %s, message: %s",
+                      event.subject, event.message)
